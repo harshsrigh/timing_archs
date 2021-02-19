51a5d7363f61eed3b0a7fdb1ea6813e40aeb023c
Harsh Shukla <harshsrigh@gmail.com>
2021-02-19 13:52:58 +0530

    changed for generating o221ai_1x timing.lib file

--- config.py
+++ config.py
@@ -3,22 +3,22 @@ from os import path
 # File Configurations
 library_directory = ''
 library_file = path.join(library_directory, 'sky130nm.lib')
-cell_directory = 'custom_stdcell/a22oi_1x/'
-spice_file = path.join(cell_directory, "a22oi_1x.spice")
+cell_directory = 'custom_stdcell/o221ai_1x/'
+spice_file = path.join(cell_directory, "o221ai_1x.spice")
 output_folder =  path.join(cell_directory, "data")
 
 # Simulation Setup
 VDD = '1.8'
 time_unit = 1e-9 # 1ns
 cap_unit = 1e-12 # 1pF
-temp = '25C'
+temp = '25C' # TODO: Need to add this into the timing_harness.cir
 # With 0.01ns it take approx 2min for 7*7 cases
-sim_step = '0.1n' # Controls the speed of Characterization (0.1n for faster results-> less accurate)
+sim_step = '0.01n' # Controls the speed of Characterization (make sure to have step less than the minimum Input slew rate)
 # Y Considered as Output 
 
 # Input Vectors
 input_delay = '0.01n 0.023n 0.0531329n 0.122474n 0.282311n 0.650743n 1.5n' # Only put the unit(do not include sec suffix)
 output_caps = '0.0005p 0.0012105800p 0.002931p 0.00709641p 0.0171815p 0.0415991p 0.100718p' # Only put the unit(do not include Farad suffix)
-input_pins = 'a0 a1 b0 b1' # TODO: extract from .lef files
-output_pins = 'y' # TODO: extract from .lef files
-logic_function = 'not ((a0 and a1) or (b0 and b1))' # Use keyword 'not', 'and' , 'or'
+input_pins = 'A1 A2 B1 B2 C1' # TODO: extract from .lef files
+output_pins = 'Y' # TODO: extract from .lef files
+logic_function = 'not ((A1 or A2) and (B1 or B2) and C1)' # Use keyword 'not', 'and' , 'or'
